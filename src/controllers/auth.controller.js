3aba3fbbfab28df3b286820e3f0eb3d6014a86b3
Alejandro Borges <avrborges@gmail.com>
2025-10-27 14:53:28 -0300

    login + logout

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import Usuario from "../models/usuario.model.js";
 import { createAccessToken } from "../libs/jwt.js";
+import bcrypt from "bcryptjs";
 
 export const registro = async (req, res) => {
 
@@ -33,7 +34,54 @@ export const registro = async (req, res) => {
   }
 };
 
+export const login = async (req, res) => {
 
-export const login = (req, res) => {
-    res.send('login')
+  const { correo, password } = req.body;
+ 
+  try {
+    const usuarioEncontrado = await Usuario.findOne({ correo });
+
+    if (!usuarioEncontrado) {
+      return res.status(404).json({
+        mensaje: 'Usuario no encontrado'
+      });
+    }
+
+    const esPasswordValido = await bcrypt.compare(password, usuarioEncontrado.password);
+
+    if (!esPasswordValido) {
+      return res.status(401).json({
+        mensaje: 'Usuario y/o contraseña incorrecta'
+      });
+    }
+
+    const token = await createAccessToken({ id: usuarioEncontrado._id });
+    res.cookie('token', token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: 'strict',
+      maxAge: 3600000 
+    });
+
+    res.status(201).json({
+      mensaje: 'Usuario logueado exitosamente',
+      id: usuarioEncontrado._id,
+      nombre: usuarioEncontrado.nombre,
+      correo: usuarioEncontrado.correo, 
+      rol: usuarioEncontrado.rol,
+      creadoEn: usuarioEncontrado.creadoEn      
+    });
+  } catch (error) {
+    res.status(400).json({
+      mensaje: 'Error al loguear usuario',
+      error: error.message
+    });
+  }
 };
+
+export const logout = (req, res) => {
+  res.clearCookie('token');
+  res.status(200).json({
+    mensaje: 'Usuario deslogueado exitosamente'
+  });
+}
--- src/routes/auth.router.js
+++ src/routes/auth.router.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { registro, login } from "../controllers/auth.controller.js"
+import { registro, login, logout } from "../controllers/auth.controller.js"
 
 const router = Router();
 
 router.post('/registro', registro)
 router.post('/login', login)
+router.post('/logout', logout)
+
 
 export default router
